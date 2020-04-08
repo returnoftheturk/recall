cf4b736ef30e6f7d8d206ac0f80e4be0706480c2
Ahmet Akgul <ahmet.akgul@nuance.com>
2020-04-08 12:44:19 -0400

    Add material ui

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {withAuthorization} from '../Session';
+import Button from '@material-ui/core/Button';
 
 const HomePage = () => (
     <div>
         <h1>Home Page</h1>
         <p> The Home Page is accessible by every signed in user.</p>
+        <Button variant="contained" color="primary">
+            Hello World
+        </Button>
     </div>
 );
 const condition = authUser => authUser != null;
