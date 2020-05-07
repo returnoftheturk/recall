004ece66658e14d496f10511c6ea91a79b160442
Ahmet Akgul <ahmet.akgul@nuance.com>
2020-05-07 00:27:10 -0400

    Almost done name card

--- src/components/custom_components/NameCard/index.js
+++ src/components/custom_components/NameCard/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import icon from '../../../css/icons/256_15.png'
+import atIcon from '../../../css/icons/atIcon.png'
 
 class ContactCard extends Component {
     constructor(props){
@@ -25,15 +26,17 @@ class ContactCard extends Component {
     render(){
         const styles = {
             frontCard: {
-                opacity: this.state.isHovering? '0.7': '1',
-                backgroundColor: this.state.isHovering? '#ffffff !important' : ''
+                opacity: this.state.isHovering? '0.6': '1',
+                backgroundColor: this.state.isHovering? '#ffffff !important' : '',
+                transition: '0.2s linear'
+
             }
         }
         return (
             <div className="cardContainer" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
                 <div className="contactCard" style={styles.frontCard}>
                     <div className="contactIcon">
-                        <img src={icon}/>
+                        <img src={icon} alt="profileIcon"/>
                         <div className="logo"><span className="fat">E</span><span className="skinny">K</span></div>		
                     </div>
                 </div>
@@ -47,14 +50,14 @@ class ContactCard extends Component {
                             Ahmet AKgul
                         </div>
                     </div>
-                    <div className="infoMiddleRow">
-                        {/* This is an example description of what 
-                        the person might have written maybe some onMouseEntert
-                        test of whta */}
+                    <div className="infoDescription">
+                        This is an example description of what 
+                        the person might have written maybe some onMouseE
     
                     </div>
-                    <div className={`${true&&'title-white'} infoBottomRow`}>
-                        @something
+                    <div className={`${true&&'title-white'} infoSocials`}>
+                        <img src={atIcon} alt="atIcon" className="atIcon"/>
+                         something
                     </div>
                 </div>            
 
