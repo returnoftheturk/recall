ff2911f0ec84d4b310e902b75e07698cfaefa97d
Ahmet Akgul <ahmet.akgul@nuance.com>
2020-06-10 23:32:26 -0400

    top row of card

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import PasswordChangeForm from '../PasswordChange';
 import {withAuthorization, AuthUserContext} from '../Session';
+import '../../css/profileCard.css';
+import n_1 from '../../css/icons/n_1.png';
 
-const AccountPage = () => (
+const AccountPage = (props) => (
     <AuthUserContext.Consumer>
         {authUser => (
-            <div>
-                <h1>Account: {authUser.email}</h1>
-                <PasswordChangeForm/>
+            <div className="profileContainer" >
+                <div className="profileCard" >
+                    <div className="profileTopRow">
+                        <img src={n_1} alt="profileIcon" className="icon"/>
+                        <div 
+                            className="profileName" 
+                            style={{background:'#6acd79'}}
+                        >
+                            AHMET
+                            AKGUL
+                        </div>		
+                    </div>
+                    <div className="profileDescription"/>
+                    <div className="profileBottomRow"/>
+                </div>
             </div>
+            // <div className="profile">
+            //     <h1>Account: {authUser.email}</h1>
+
+            //     <PasswordChangeForm/>
+            // </div>
         )}
     </AuthUserContext.Consumer>
 
