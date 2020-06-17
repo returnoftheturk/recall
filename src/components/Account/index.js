1bd9cd16246ecb01ad38277447ceedaae57a088e
Ahmet Akgul <ahmet.akgul@nuance.com>
2020-06-17 01:21:28 -0400

    Some profile card work

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import PasswordChangeForm from '../PasswordChange';
+import React, { Component } from 'react';
 import {withAuthorization, AuthUserContext} from '../Session';
 import '../../css/profileCard.css';
 import n_1 from '../../css/icons/n_1.png';
+import Alert from '@material-ui/lab/Alert';
+import Button from '@material-ui/core/Button';
 
-const AccountPage = (props) => (
-    <AuthUserContext.Consumer>
-        {authUser => (
+class AccountPage extends Component {
+    constructor(props){
+        super(props);
+        this.handleForgotEmail = this.handleForgotEmail.bind(this);
+        this.state = {
+            emailSent: false,
+            closed: false,
+            error: false
+        }
+    }
+
+    handleForgotEmail(){
+        const authUser = this.context;
+        const email = authUser.email;
+        this.props.firebase.doPasswordReset(email).then(()=>{
+            this.setState({emailSent: true, closed: false});
+        }).catch(err=>{
+            this.setState({error:true, closed:false});
+            console.log(err);
+        })
+    }
+    render(){
+        const authUser = this.context;
+        const {emailSent, closed, error} = this.state;
+        return(
             <div className="profileContainer" >
+                {emailSent && !closed && 
+                    <Alert severity="success"
+                        onClose={()=>{this.setState({closed: true})}}
+                    >
+                            Password reset email successfully sent to: {authUser.email}
+                    </Alert>
+                }
+                {error && !closed && 
+                    <Alert 
+                        severity="error"
+                        onClose={()=>{this.setState({closed: true})}}
+                    >
+                        Could not send password reset email                    
+                    </Alert>}
                 <div className="profileCard" >
                     <div className="profileTopRow">
                         <img src={n_1} alt="profileIcon" className="icon"/>
@@ -19,19 +56,22 @@ const AccountPage = (props) => (
                             AKGUL
                         </div>		
                     </div>
-                    <div className="profileDescription"/>
-                    <div className="profileBottomRow"/>
+                    <div className="profileDescription">
+                        <div>
+                            {authUser.email}
+                        </div>
+                        
+                        <Button color="secondary" onClick={this.handleForgotEmail}>
+                            Forgot Password?
+                        </Button>
+                    </div>
                 </div>
             </div>
-            // <div className="profile">
-            //     <h1>Account: {authUser.email}</h1>
+        )
+    }
 
-            //     <PasswordChangeForm/>
-            // </div>
-        )}
-    </AuthUserContext.Consumer>
-
-);
+}
+AccountPage.contextType = AuthUserContext;
 const condition = authUser => authUser != null;
 
 export default withAuthorization(condition)(AccountPage);
