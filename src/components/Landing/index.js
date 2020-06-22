54985b7c6c038f412a55b47d8e3a85046321aa8c
Ahmet Akgul <ahmet.akgul@nuance.com>
2020-06-21 22:05:07 -0400

    Fix landing view

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+import searchImage from '../../css/icons/landing.jpg';
+import Button from '@material-ui/core/Button';
 
-const Landing = () => (
-    <div>
-        <h1>App</h1>
-        <ul>
-            <li>
-                <Link to = {ROUTES.SIGN_IN}> Sign In</Link>
-            </li>        
-        </ul>
-    </div>
-);
+class Landing extends Component{
+    handleLoginClick = () =>{
+        this.props.history.push(ROUTES.SIGN_IN);
+    }
+    render(){
+        return (
+            <div className="emptyContainer">
+                <div className="logoTitle">RECALL</div>
+                <img className="emptyBackground" src={searchImage} alt="zero"/>
+                <div className="emptyMessage">
+                    Never forget a name again.
+                </div>
+                <Button color="primary" onClick={this.handleLoginClick}>
+                    GET STARTED
+                </Button>
+            </div>
+        )
+    }
+}
 
-export default Landing;
+export default withRouter(Landing);
